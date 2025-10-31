9dc5621a757e9296e0d05720f429de59c37d1a6a
arunkumar938 <arunkunjuttan2233@gmail.com>
2025-10-31 16:51:43 +0530

    BasicCalculator

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -3,9 +3,9 @@ import { useState } from 'react';
 import { Button, Form, Row,Col } from "react-bootstrap";
 
 function Calculator() {
-    const [num1, setNum1] = useState(0);
-    const [num2, setNum2] = useState(0);
-    const [result, setResult] = useState(0);
+    const [num1, setNum1] = useState("");
+    const [num2, setNum2] = useState("");
+    const [result, setResult] = useState("");
 
     const calculate = (operator) => {
         const a = parseFloat(num1);
